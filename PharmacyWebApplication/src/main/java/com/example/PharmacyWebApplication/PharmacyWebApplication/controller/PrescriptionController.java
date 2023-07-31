3475aae625a1e6723a98e891c6f4fd40438a5e4b
Hansini Abeygunwardhana <hchathupomoda@gmail.com>
2023-07-31 20:27:23 +0530

    prescription retrieve api

--- PharmacyWebApplication/src/main/java/com/example/PharmacyWebApplication/PharmacyWebApplication/controller/PrescriptionController.java
+++ PharmacyWebApplication/src/main/java/com/example/PharmacyWebApplication/PharmacyWebApplication/controller/PrescriptionController.java
@@ -2,8 +2,6 @@ package com.example.PharmacyWebApplication.PharmacyWebApplication.controller;
 
 import com.example.PharmacyWebApplication.PharmacyWebApplication.common.ApiResponse;
 import com.example.PharmacyWebApplication.PharmacyWebApplication.dto.PrescriptionDto;
-import com.example.PharmacyWebApplication.PharmacyWebApplication.dto.ProductDto;
-import com.example.PharmacyWebApplication.PharmacyWebApplication.model.Category;
 import com.example.PharmacyWebApplication.PharmacyWebApplication.model.Prescription;
 import com.example.PharmacyWebApplication.PharmacyWebApplication.service.FileService;
 import com.example.PharmacyWebApplication.PharmacyWebApplication.service.PrescriptionService;
@@ -14,10 +12,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/prescriptions")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PrescriptionController {
 
     @Autowired
@@ -38,6 +37,17 @@ public class PrescriptionController {
         } catch (IOException e) {
             return new ResponseEntity<>(new ApiResponse(false, "Error uploading prescription details"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<PrescriptionDto>> getPrescriptions(){
+        List<PrescriptionDto> prescriptions = prescriptionService.getAllPrescriptions();
+        return new ResponseEntity<>(prescriptions,HttpStatus.OK);
+    }
+
+    @GetMapping({"/getPrescriptionDetails"})
+    public List<Prescription> getCartDetails() {
+        return prescriptionService.getPrescriptionDetails();
     }
 
 }
--- PharmacyWebApplication/src/main/java/com/example/PharmacyWebApplication/PharmacyWebApplication/service/PrescriptionService.java
+++ PharmacyWebApplication/src/main/java/com/example/PharmacyWebApplication/PharmacyWebApplication/service/PrescriptionService.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PrescriptionService {
@@ -45,4 +47,26 @@ public class PrescriptionService {
         prescriptionRepository.save(prescription);
     }
 
+    public PrescriptionDto getPrescriptionDto(Prescription prescription){
+        PrescriptionDto prescriptionDto = new PrescriptionDto();
+        prescriptionDto.setFrequency(prescription.getFrequency());
+        prescriptionDto.setPrescriptionImageUrl(prescription.getPrescriptionImageUrl());
+        prescriptionDto.setFulfilment(prescription.getFulfilment());
+        prescriptionDto.setSubstitute(prescription.getSubstitute());
+        return prescriptionDto;
+    }
+    public List<PrescriptionDto> getAllPrescriptions() {
+        List<Prescription> allPrescriptions = prescriptionRepository.findAll();
+        List<PrescriptionDto> prescriptionDtos = new ArrayList<>();
+        for (Prescription prescription:allPrescriptions){
+            prescriptionDtos.add(getPrescriptionDto(prescription));
+        }
+        return prescriptionDtos;
+    }
+
+    public List<Prescription> getPrescriptionDetails() {
+        String username = JwtRequestFilter.CURRENT_USER;
+        User user = userDao.findById(username).get();
+        return prescriptionRepository.findByUser(user);
+    }
 }
